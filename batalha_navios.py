e9d9ecd2d6c2a667df14de078c2074922c1b95b2
AgathaLice <miguelfabrisd@gmail.com>
2024-12-05 16:52:35 -0300

    mudança a navio
    criação do início do código

--- batalha_navios.py
+++ batalha_navios.py
@@ -1,3 +1,18 @@
+def definir_vida(formato):
+    vida = 0
+    for linhas in formato:
+        for itens in linhas:
+            if itens == "H":
+                vida += 1
+    return vida
+
 class Navio:
-    def __init__(self, tamanho):
-        self.tamanho = tamanho
+    def __init__(self, formato):
+        self.formato = formato
+        self.vida = definir_vida(formato)
+navio1 = Navio([
+    ["H"], 
+    ["H"], 
+    ["H"]
+    ])
+print(navio1.vida)
