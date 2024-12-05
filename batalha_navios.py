a64c1a3593d7681089272c445685c02a72b20886
AgathaLice <miguelfabrisd@gmail.com>
2024-12-05 18:57:51 -0300

    criação de todos os navios do jogo
    a classe de navios está pronta conforme o que eu pensei

--- batalha_navios.py
+++ batalha_navios.py
@@ -2,17 +2,35 @@ def definir_vida(formato):
     vida = 0
     for linhas in formato:
         for itens in linhas:
-            if itens == "H":
+            if itens == "X":
                 vida += 1
     return vida
 
 class Navio:
-    def __init__(self, formato):
+    def __init__(self, formato, quantidade):
         self.formato = formato
         self.vida = definir_vida(formato)
-navio1 = Navio([
-    ["H"], 
-    ["H"], 
-    ["H"]
-    ])
-print(navio1.vida)
+        self.quantidade = quantidade
+
+
+navio_1x2 = Navio([["X", "X"]], 3)
+navio_2x1 = Navio([ ["X"], 
+                    ["X"]], 3)
+navio_3x1 = Navio([ ["X"], 
+                    ["X"], 
+                    ["X"]], 2)
+navio_4x1 = Navio([ ["X"], 
+                    ["X"], 
+                    ["X"],
+                    ["X"]], 1)
+navio_2x2 = Navio([ ["X", "X"], 
+                    ["X", "X"]], 1)
+navio_T = Navio([  ["X", "X", "X"], 
+                        ["X"], 
+                        ["X"],
+                        ["X"]], 1)
+navio_L = Navio([   ["X"], 
+                    ["X"], 
+                    ["X"],
+                    ["X"], ["X"]], 1)
+
