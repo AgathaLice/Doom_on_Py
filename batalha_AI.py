f366426cdb038ececdc30ffd45ac11b0b9d99cc1
AgathaLice <miguelfabrisd@gmail.com>
2024-12-05 11:27:56 -0300

    começo navio

--- batalha_AI.py
+++ batalha_AI.py
@@ -1,1 +1,8 @@
-print("OLAAA")
+class Navio:
+    def definir_vida(formato):
+        for linhas in formato:
+            for itens in linhas:
+                if itens == 
+
+    def __init__(self, formato):
+        self.formato = formato
