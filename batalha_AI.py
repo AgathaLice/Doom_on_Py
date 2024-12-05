68ec03da81e7aeecb38bc0df856bebae7cff5d29
AgathaLice <miguelfabrisd@gmail.com>
2024-12-05 11:28:10 -0300

    começo navio

--- batalha_AI.py
+++ batalha_AI.py
@@ -1,8 +1,12 @@
-class Navio:
-    def definir_vida(formato):
+def definir_vida(formato):
         for linhas in formato:
             for itens in linhas:
-                if itens == 
+                if itens == "H":
+                    vida += 1
 
+class Navio:
     def __init__(self, formato):
         self.formato = formato
+        self.vida = definir_vida(formato)
+navio1 = 
+print()
